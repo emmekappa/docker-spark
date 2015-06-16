1a08805efd73427a3fd66fa240cf74da6418bd0a
Michele Cantelli <emmekappa@gmail.com>
2015-06-16 21:33:52 +0000

    Supporting docker wave interface

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-#export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-#export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-export SPARK_LOCAL_IP=0.0.0.0
+ETHWE_IP=`ifconfig ethwe 2>/dev/null | perl -nle 's/dr:(\S+)/print $1/e'`
+export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+[ "$ETHWE_IP" ] && export SPARK_MASTER_IP=$ETHWE_IP
+[ "$ETHWE_IP" ] && export SPARK_LOCAL_IP=$ETHWE_IP
 /usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP "$@"
 tail -f /dev/null
