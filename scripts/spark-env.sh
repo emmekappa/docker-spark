356b9a869d99db03de448797ea13bcfcf6e1d37e
Michele Cantelli <michele.cantelli@yoox.com>
2015-07-08 14:51:59 +0200

    Waiting for ethwe: the chic way.

--- scripts/spark-env.sh
+++ scripts/spark-env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 #wait for ethwe 
-sleep 3
+while [ ! "$(ifconfig | grep ^ethwe | grep \<UP,)" ]; do sleep 1; echo ethwe is down; done
 ETHWE_IP=`ifconfig ethwe 2>/dev/null | perl -nle 's/dr:(\S+)/print $1/e'`
 
 [ "$SPARK_MASTER_PORT_7077_TCP_ADDR" ] && export SPARK_MASTER_IP=$SPARK_MASTER_PORT_7077_TCP_ADDR
