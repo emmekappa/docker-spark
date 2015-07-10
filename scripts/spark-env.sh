bcc15631457b5ceac76d15ff69b9655417036c70
Michele Cantelli <michele.cantelli@yoox.com>
2015-07-10 17:42:30 +0200

    Waiting for ethwe interface just if SPARK_MASTER environment variable is not set

--- scripts/spark-env.sh
+++ scripts/spark-env.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 #wait for ethwe 
-while [ ! "$(ifconfig -s | grep ethwe)" ]; do sleep 1; echo ethwe is down; done
-ETHWE_IP=`ifconfig ethwe 2>/dev/null | perl -nle 's/dr:(\S+)/print $1/e'`
+if [ ! "$SPARK_MASTER" ]; then
+	while [ ! "$(ifconfig -s | grep ethwe)" ]; do sleep 1; echo ethwe is down; done
+	ETHWE_IP=`ifconfig ethwe 2>/dev/null | perl -nle 's/dr:(\S+)/print $1/e'`
+fi
 
 [ "$SPARK_MASTER_PORT_7077_TCP_ADDR" ] && export SPARK_MASTER_IP=$SPARK_MASTER_PORT_7077_TCP_ADDR
 [ "$ETHWE_IP" ] && export SPARK_LOCAL_IP=$ETHWE_IP
