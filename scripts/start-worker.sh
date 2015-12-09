a7436821e1930768f9107e63fa36b3034fd4d02b
Michele Cantelli <michele.cantelli@yoox.com>
2015-12-09 02:58:30 +0100

    port

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -3,6 +3,6 @@ source spark-env.sh
 cd /usr/local/spark
 ./bin/spark-class org.apache.spark.deploy.worker.Worker \
 	spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_PORT_7077_TCP_PORT} \
-#	--properties-file /spark-defaults.conf \
+	--properties-file /spark-defaults.conf \
 #	-i $SPARK_LOCAL_IP \
 	"$@"
