8aec6b91acaa636f3c5c467dc18651563598ae9e
Michele Cantelli <michele.cantelli@yoox.com>
2015-12-09 02:14:26 +0100

    Updating start master

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -2,5 +2,5 @@
 
 source spark-env.sh
 
-/usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP "$@"
+/usr/local/spark/sbin/start-master.sh #--properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP "$@"
 tail -f /dev/null
